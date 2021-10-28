6be1a511ee8ef93fa9c571415bbc3c1a366da420
lixingleon <tomatogo@outlook.com>
2021-10-28 12:22:05 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import StudentComponent from "./components/StudentComponent";
 import TrainDataComponent from "./components/TrainDataComponent";
 
 function App() {
   return (
     <div className="App">
-      {/*<StudentComponent/>*/}
         <TrainDataComponent/>
     </div>
   );
