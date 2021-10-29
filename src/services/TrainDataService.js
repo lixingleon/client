8f4935062e293ef7b9671fcf8280a40752fcf08b
lixingleon <tomatogo@outlook.com>
2021-10-29 11:19:51 -0700

    add new end point

--- src/services/TrainDataService.js
+++ src/services/TrainDataService.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
 
-const TrainData_URL = 'http://localhost:8080/api/TrainData'
+const TrainData_URL = 'http://localhost:8080/api/TrainData';
+const Train_URL = 'http://localhost:8080/api/Train';
 
 class TrainDataService{
 
     getTrainResult(data){
         console.log(data)
-        return axios.post(TrainData_URL, data);
+        return axios.post(Train_URL, data);
     }
     getUploadedFiles(){
         return axios.get(TrainData_URL);
